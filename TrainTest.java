15a947a3a62220597751733faee32132eba8ee66
esommerfeld2 <esommerfeld@smith.edu>
2025-03-05 16:02:26 -0500

    half done

--- TrainTest.java
+++ TrainTest.java
@@ -7,18 +7,25 @@ public class TrainTest {
 
     //Atributes
     Engine eng;
+    Car carC;
+    Passenger ella;
+    Train trainA;
 
     //Before Methods
     @Before
     public void setup(){
        this.eng = new Engine(FuelType.ELECTRIC, 50, 95);
+       this.carC = new Car(" C", 2);
+       this.ella = new Passenger("Ella");
+       this.trainA = new Train(FuelType.STEAM, 100, 100, 4, 2);
    }
 
     // Engine Tests
     @Test
     public void testEngineConstructor() {
+        double expectedFuel = 50;
         assertEquals(FuelType.ELECTRIC,eng.getFuelType());
-        //fuel level when private?
+        //assertEquals(expectedFuel, eng.getCurrentFuel());
     }
 
     @Test
@@ -29,12 +36,13 @@ public class TrainTest {
     // Car Tests
     @Test
     public void testCarAddPassenger() {
-        fail();
+        assertTrue(carC.addPassenger(ella));
     }
 
     @Test
     public void testCarRemovePassenger() {
-        fail();
+        carC.addPassenger(ella);
+        assertTrue(carC.removePassenger(ella));
     }
 
     // Passenger Tests
